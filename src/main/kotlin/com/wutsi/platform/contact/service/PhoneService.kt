3aeb5726ba52d95ed96ab6dbcf32769e2780a116
wutsi <herve.tchepannou@wutsi.com>
2022-02-08 11:09:13 -0500

    improve logging

--- src/main/kotlin/com/wutsi/platform/contact/service/PhoneService.kt
+++ src/main/kotlin/com/wutsi/platform/contact/service/PhoneService.kt
@@ -3,6 +3,7 @@ package com.wutsi.platform.contact.service
 import com.wutsi.platform.contact.dao.PhoneRepository
 import com.wutsi.platform.contact.entity.PhoneEntity
 import com.wutsi.platform.contact.event.SyncRequestPayload
+import com.wutsi.platform.core.logging.KVLogger
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
@@ -10,13 +11,15 @@ import javax.transaction.Transactional
 class PhoneService(
     private val dao: PhoneRepository,
     private val phoneNumberSanitizer: PhoneNumberSanitizer,
-    private val tenantProvider: TenantProvider
+    private val tenantProvider: TenantProvider,
+    private val logger: KVLogger,
 ) {
     @Transactional
     fun addPhone(payload: SyncRequestPayload): PhoneEntity? {
         val tenant = tenantProvider.get()
         val xphoneNumber = phoneNumberSanitizer.sanitize(payload.phoneNumber, tenant)
             ?: return null
+        logger.add("phone_number_formatter", xphoneNumber)
 
         val opt = dao.findByAccountIdAndNumberAndTenantId(payload.accountId, xphoneNumber, tenant.id)
         if (opt.isPresent)
